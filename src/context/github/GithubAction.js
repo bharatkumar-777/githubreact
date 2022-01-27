1c1fd6b734e693e85e99ba32cd2a3a43ea0de896
prince <princesapra313@gmail.com>
2022-01-28 00:51:22 +0530

    env change

--- src/context/github/GithubAction.js
+++ src/context/github/GithubAction.js
@@ -28,10 +28,10 @@ export const searchUsers = async (text) => {
 
   export const getUserandRepos = async (login) => {
   
-   const params= new URLSearchParams({
-        sort:'created',
-        per_page: 10,
-    });
+  //  const params= new URLSearchParams({
+  //       sort:'created',
+  //       per_page: 10,
+  //   });
 
     const [user,repos]=await Promise.all([
         github.get(`/users/${login}`),
